5a60637121759dace2a7e2a14d1fcac19f6defdd
jinparksj <jinparksj@gmail.com>
2019-01-20 23:21:31 -0800

    custom slot with slider, dial, label

--- 09_08_with_methods.py
+++ 09_08_with_methods.py
@@ -1,28 +1,34 @@
 import sys
 
+from PyQt5.QtWidgets import QWidget
 from PyQt5.QtWidgets import QDial
 from PyQt5.QtWidgets import QSlider
-from PyQt5.QtWidgets import QWidget
+from PyQt5.QtWidgets import QLineEdit
 from PyQt5.QtWidgets import QBoxLayout
 from PyQt5.QtWidgets import QApplication
-from PyQt5.QtWidgets import QLineEdit
+
 from PyQt5.QtCore import Qt
 from PyQt5.QtCore import pyqtSlot
 
 class CustomSlider(QSlider):
-    def __init__(self, *args):
+    def __init__(self, *args, **kwargs):
         QSlider.__init__(self, *args)
 
-    @pyqtSlot(int)
-    @pyqtSlot(str) # decorator can make the function making input as int and str as function overloading
-    def setValue(self, value):
-        value = int(value)
+    @pyqtSlot(int, name="setValue")
+    def set_int_value(self, value):
+        QSlider.setValue(self, value)
+
+    @pyqtSlot(str, name="setValue")
+    def set_str_value(self, value):
+        try:
+            value = int(value)
+        except Exception:
+            value = 0
         QSlider.setValue(self, value)
 
 class Form(QWidget):
     def __init__(self):
         QWidget.__init__(self, flags=Qt.Widget)
-        self.cnt = 0
         self.le = QLineEdit()
         self.dial = QDial()
         self.sld = CustomSlider(Qt.Horizontal)
@@ -31,13 +37,13 @@ class Form(QWidget):
 
     def init_widget(self):
         self.setWindowTitle("custom slot")
-        form_lbx = QBoxLayout(QBoxLayout.TopToBottom, parent = self)
-        control_lbx = QBoxLayout(QBoxLayout.LeftToRight, parent = self)
+        form_lbx = QBoxLayout(QBoxLayout.TopToBottom, parent=self)
+        control_lbx = QBoxLayout(QBoxLayout.LeftToRight, parent=self)
         self.setLayout(form_lbx)
 
         self.le.setMaximumWidth(40)
 
-        #connect signal and slot
+        #connect between signal and slot
         self.sld.valueChanged.connect(self.valueChanged)
         self.le.textChanged.connect(self.sld.setValue)
         self.dial.valueChanged.connect(self.sld.setValue)
@@ -48,13 +54,18 @@ class Form(QWidget):
         control_lbx.addWidget(self.le)
 
 
-    @pyqtSlot(int, name = "valueChanged")
+    @pyqtSlot(int, name="valueChanged")
     def value_changed(self, value):
         self.le.setText(str(value))
         self.dial.setValue(value)
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     form = Form()
     form.show()
     exit(app.exec())
+
+
+
+
